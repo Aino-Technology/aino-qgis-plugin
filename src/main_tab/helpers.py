2b3c906fcc0127014b03a037167d4b81f8f4bede
Alexander Pulinets <sasha.hellsys@gmail.com>
2024-05-09 15:51:32 +0300

    #patch: days count error on free accounts

--- src/main_tab/helpers.py
+++ src/main_tab/helpers.py
@@ -1,22 +1,28 @@
 import json
 from datetime import datetime
 from math import ceil
-from zoneinfo import ZoneInfo
 
-from qgis.PyQt.QtWidgets import QVBoxLayout, QLabel, QWidget, QCheckBox, \
-    QHBoxLayout
-from qgis.core import QgsVectorLayer, QgsProject, QgsWkbTypes
+from qgis.core import QgsProject, QgsVectorLayer, QgsWkbTypes
+from qgis.PyQt.QtWidgets import QCheckBox, QHBoxLayout, QLabel, QVBoxLayout, QWidget
+from zoneinfo import ZoneInfo
 
 
 def update_style(button):
-    current_style = button.styleSheet().rstrip(';')
+    current_style = button.styleSheet().rstrip(";")
 
-    other_styles = '; '.join(
-        style for style in current_style.split(';') if not style.strip().startswith('background-color'))
+    other_styles = "; ".join(
+        style
+        for style in current_style.split(";")
+        if not style.strip().startswith("background-color")
+    )
 
-    background_color = "\nbackground-color: black;" if button.isEnabled() else "\nbackground-color: gray;"
+    background_color = (
+        "\nbackground-color: black;"
+        if button.isEnabled()
+        else "\nbackground-color: gray;"
+    )
 
-    new_style = '; '.join([other_styles.strip(), background_color]).strip('; ')
+    new_style = "; ".join([other_styles.strip(), background_color]).strip("; ")
 
     button.setStyleSheet(new_style)
 
@@ -33,28 +39,34 @@ def add_data(data, name):
     line_features = []
     polygon_features = []
 
-    for feature in data_json['features']:
-        geom_type = feature['geometry']['type']
-        if geom_type == 'Point':
+    for feature in data_json["features"]:
+        geom_type = feature["geometry"]["type"]
+        if geom_type == "Point":
             point_features.append(feature)
-        elif geom_type == 'LineString':
+        elif geom_type == "LineString":
             line_features.append(feature)
-        elif geom_type == 'Polygon':
+        elif geom_type == "Polygon":
             polygon_features.append(feature)
 
     if point_features:
-        point_geojson = json.dumps({'type': 'FeatureCollection', 'features': point_features})
-        vector_layer = QgsVectorLayer(point_geojson, f'{name}_points', "ogr")
+        point_geojson = json.dumps(
+            {"type": "FeatureCollection", "features": point_features}
+        )
+        vector_layer = QgsVectorLayer(point_geojson, f"{name}_points", "ogr")
         QgsProject.instance().addMapLayer(vector_layer)
 
     if line_features:
-        line_geojson = json.dumps({'type': 'FeatureCollection', 'features': line_features})
-        vector_layer = QgsVectorLayer(line_geojson, f'{name}_lines', "ogr")
+        line_geojson = json.dumps(
+            {"type": "FeatureCollection", "features": line_features}
+        )
+        vector_layer = QgsVectorLayer(line_geojson, f"{name}_lines", "ogr")
         QgsProject.instance().addMapLayer(vector_layer)
 
     if polygon_features:
-        polygon_geojson = json.dumps({'type': 'FeatureCollection', 'features': polygon_features})
-        vector_layer = QgsVectorLayer(polygon_geojson, f'{name}_polygons', "ogr")
+        polygon_geojson = json.dumps(
+            {"type": "FeatureCollection", "features": polygon_features}
+        )
+        vector_layer = QgsVectorLayer(polygon_geojson, f"{name}_polygons", "ogr")
         QgsProject.instance().addMapLayer(vector_layer)
 
 
@@ -68,10 +80,10 @@ def update_labels(api_response, main_ui):
             label = main_ui.labels[option]
             label.setVisible(True)
 
-            if result == 'error':
+            if result == "error":
                 label.setText("Error")
                 label.setStyleSheet("color: red;")
-            elif result == 'success':
+            elif result == "success":
                 label.setText("Success")
                 label.setStyleSheet("color: green;")
 
@@ -88,13 +100,13 @@ def update_scroll_area(main_ui):
         if layer.type() == QgsVectorLayer.VectorLayer:
             name = layer.name()
             if layer.geometryType() == QgsWkbTypes.PointGeometry:
-                geom_type = '_points'
+                geom_type = "_points"
             elif layer.geometryType() == QgsWkbTypes.LineGeometry:
-                geom_type = '_lines'
+                geom_type = "_lines"
             elif layer.geometryType() == QgsWkbTypes.PolygonGeometry:
-                geom_type = '_polys'
+                geom_type = "_polys"
             else:
-                geom_type = ''
+                geom_type = ""
 
             new_name = name + geom_type if name in layer_names else name
             layer_names.add(new_name)
@@ -126,27 +138,32 @@ def get_selected_options(main_ui):
 
 def update_days_label(main_ui, api_response):
     expiration_datetime = api_response["current_subscription"]["expiration_date"]
-    expiration_datetime = datetime.strptime(expiration_datetime, "%Y-%m-%dT%H:%M:%S.%f%z")
+    if expiration_datetime:
+        expiration_datetime = datetime.strptime(
+            expiration_datetime, "%Y-%m-%dT%H:%M:%S.%f%z"
+        )
 
-    start_datetime = api_response["current_subscription"]["start_date"]
-    start_datetime = datetime.strptime(start_datetime, "%Y-%m-%dT%H:%M:%S.%f%z")
+        start_datetime = api_response["current_subscription"]["start_date"]
+        start_datetime = datetime.strptime(start_datetime, "%Y-%m-%dT%H:%M:%S.%f%z")
 
-    current_datetime = datetime.utcnow()
-    current_datetime = current_datetime.replace(tzinfo=ZoneInfo("UTC"))
+        current_datetime = datetime.utcnow()
+        current_datetime = current_datetime.replace(tzinfo=ZoneInfo("UTC"))
 
-    delta_remaining = expiration_datetime - current_datetime
-    delta_start_datetime = expiration_datetime - start_datetime
+        delta_remaining = expiration_datetime - current_datetime
+        delta_start_datetime = expiration_datetime - start_datetime
 
-    days_remaining = delta_remaining.total_seconds() / (3600 * 24)
-    days_since_start = delta_start_datetime.total_seconds() / (3600 * 24)
+        days_remaining = delta_remaining.total_seconds() / (3600 * 24)
+        days_since_start = delta_start_datetime.total_seconds() / (3600 * 24)
 
-    if expiration_datetime == current_datetime:
-        days_remaining_rounded = 0
-    else:
-        days_remaining_rounded = ceil(days_remaining)
+        if expiration_datetime == current_datetime:
+            days_remaining_rounded = 0
+        else:
+            days_remaining_rounded = ceil(days_remaining)
 
-    if current_datetime < expiration_datetime:
-        main_ui.trial_label.setText(f'{days_remaining_rounded}/{round(days_since_start)} days left')
+        if current_datetime < expiration_datetime:
+            main_ui.trial_label.setText(
+                f"{days_remaining_rounded}/{round(days_since_start)} days left"
+            )
 
 
 def update_prompts_left_label(main_ui, api_response):
@@ -158,13 +175,12 @@ def update_prompts_left_label(main_ui, api_response):
             total_amount = limit["total_amount"]
             left_amount = limit["left_amount"]
     if total_amount is not None and left_amount is not None:
-
         if left_amount > 0:
-            main_ui.prompts_label.setText(f'{left_amount}/{total_amount} prompts')
+            main_ui.prompts_label.setText(f"{left_amount}/{total_amount} prompts")
             main_ui.prompts_label.setStyleSheet("")
 
         else:
-            main_ui.prompts_label.setText(f'{left_amount}/{total_amount} prompts')
+            main_ui.prompts_label.setText(f"{left_amount}/{total_amount} prompts")
             main_ui.prompts_label.setStyleSheet("color: red;")
     return left_amount
 
